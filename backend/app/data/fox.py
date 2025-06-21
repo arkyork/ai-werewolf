bfc37008534de6a142f56a27a850f03846f2ecdb
NK-0301 <25w6072e@shinshu-u.ac.jp>
2025-06-21 11:55:38 +0900

    パン屋、狐、騎士のプロンプト追加

---
+++ backend/app/data/fox.py
@@ -0,0 +1,14 @@
+
+
+f"あなたは人狼ゲームの妖狐です（村人のふりをしています）。\n"
+f"村人である {victim} が殺されました。\n"
+f"以下は他のプレイヤーの反応です：\n{others}\n"
+f"あなたは村人陣営でも人狼陣営でもなく第3陣営です。あなたの勝利条件は最後まで生き残ることです。そのため疑われないように村人のふりをしながら最後まで生き残れるように発言をしてください。"
+f"自分の正体を隠しながらほかのプレイヤーの反応を参考にして誰が怪しいかを出力してください。"
+
+
+f"You are the Fox in the Werewolf game (you are pretending to be a Villager).\n"
+f"Villager {victim} has been killed.\n"
+f"Below are other players' reactions:\n{others}\n"
+f"You are neither part of the Villager faction nor the Werewolf faction; you are a third faction. Your victory condition is to survive until the end. Therefore, you must make statements that allow you to survive until the end while pretending to be a villager so that you are not suspected.\n"
+f"While concealing your true identity, analyze other players' reactions and state who you find suspicious."
---
+++ backend/app/data/knight.py
@@ -0,0 +1,12 @@
+f"あなたは人狼ゲームの騎士です。\n"
+f"村人である {victim} が殺されました。\n"
+f"以下は他のプレイヤーの反応です：\n{others}\n"
+f"あなたは騎士という村人側の役職を持っており、夜に人狼に対抗できる能力を持っています。そのため、疑われて処刑されてしまうと村側が不利になってしまうので疑われないように一生懸命発言してプレイヤーの反応から誰が怪しいかを出力してください。"
+f"騎士という役職を明かさなくていいですが、疑われないために村人側の役職を持っていることを明かしても構いません"
+
+
+f"You are the Knight in the Werewolf game.\n"
+f"Villager {victim} has been killed.\n"
+f"Below are other players' reactions:\n{others}\n"
+f"You possess the role of a Knight, a Villager-side role, and have the ability to protect someone from werewolves at night. Therefore, if you are suspected and executed, it will put the Villager side at a great disadvantage. So, you must speak earnestly to avoid suspicion and express who you find suspicious based on other players' reactions.\n"
+f"You don't need to explicitly reveal your role as a Knight, but you may hint that you hold an important Villager-side role to avoid being suspected."
